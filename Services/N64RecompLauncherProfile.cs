916472475fa312dae7f63b62e565f614bf66eb94
dibo <henscoper@gmail.com>
2026-05-11 21:00:20 +0200

    changed default game names to project names

--- Services/N64RecompLauncherProfile.cs
+++ Services/N64RecompLauncherProfile.cs
@@ -20,47 +20,47 @@ namespace N64RecompLauncher.Services
         {
             var standard = new List<object>
     {
-        new { name = "Zelda 64",
+        new { name = "Zelda 64: Recompiled",
             repository = "Zelda64Recomp/Zelda64Recomp",
             folderName = "Zelda64Recompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/Zelda64Recomp/Zelda64Recomp/refs/heads/dev/icons/512.png" },
 
-        new { name = "Goemon 64",
+        new { name = "Goemon 64: Recompiled",
             repository = "klorfmorf/Goemon64Recomp",
             folderName = "Goemon64Recompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/klorfmorf/Goemon64Recomp/refs/heads/dev/icons/512.png" },
 
-        new { name = "Mario Kart 64",
+        new { name = "Mario Kart 64: Recompiled",
             repository = "sonicdcer/MarioKart64Recomp",
             folderName = "MarioKart64Recompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/sonicdcer/MarioKart64Recomp/refs/heads/main/icons/512.png" },
 
-        new { name = "Dinosaur Planet",
+        new { name = "Dinosaur Planet: Recompiled",
             repository = "DinosaurPlanetRecomp/dino-recomp",
             folderName = "DinoPlanetRecompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/DinosaurPlanetRecomp/dino-recomp/refs/heads/main/icons/64.png" },
 
-        new { name = "Dr. Mario 64",
+        new { name = "Dr. Mario 64: Recompiled",
             repository = "theboy181/drmario64_recomp_plus",
             folderName = "drmario64_recomp",
             gameIconUrl  = "https://raw.githubusercontent.com/theboy181/drmario64_recomp_plus/refs/heads/main/icons/512.png" },
 
-        new { name = "Duke Nukem: Zero Hour",
+        new { name = "Duke Nukem: Zero Hour: Recompiled",
             repository = "sonicdcer/DNZHRecomp",
             folderName = "DNZHRecompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/sonicdcer/DNZHRecomp/refs/heads/main/icons/512.png" },
 
-        new { name = "Star Fox 64",
+        new { name = "Star Fox 64: Recompiled",
             repository = "sonicdcer/Starfox64Recomp",
             folderName = "Starfox64Recompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/sonicdcer/Starfox64Recomp/refs/heads/main/icons/512.png" },
 
-        new  { name = "Banjo 64",
+        new  { name = "Banjo: Recompiled",
             repository = "BanjoRecomp/BanjoRecomp",
             folderName = "BanjoRecompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/BanjoRecomp/BanjoRecomp/refs/heads/main/icons/app.png" },
             
-        new  { name = "Bomberman 64",
+        new  { name = "Bomberman 64: Recompiled",
             repository = "RevoSucks/BM64Recomp",
             folderName = "BM64Recompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/RevoSucks/BM64Recomp/refs/heads/master/icons/512.png" },
@@ -68,22 +68,22 @@ namespace N64RecompLauncher.Services
 
             var experimental = new List<object>
     {
-        new { name = "Chameleon Twist",
+        new { name = "Chameleon Twist: Recompiled",
             repository = "Rainchus/ChameleonTwist1-JP-Recomp",
             folderName = "ChameleonTwistRecompiled",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon/c1f22f4c38899f51f1ed3ce20120bbd9.png" },
 
-        new { name = "Mega Man 64",
+        new { name = "Mega Man 64: Recompiled",
             repository = "MegaMan64Recomp/MegaMan64Recompiled",
             folderName = "MegaMan64Recompiled",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon/850618e22f83f152773d2a3e51168812.png" },
 
-        new { name = "Quest 64",
+        new { name = "Quest 64: Recompiled",
             repository = "Rainchus/Quest64-Recomp",
             folderName = "Quest64Recompiled",
             gameIconUrl  = "https://raw.githubusercontent.com/Rainchus/Quest64-Recomp/refs/heads/main/icons/512.png" },
 
-        new { name = "Bomberman Hero",
+        new { name = "Bomberman Hero: Recompiled",
             repository = "RevoSucks/BMHeroRecomp",
             folderName = "BMHeroRecomp",
             gameIconUrl  = "https://raw.githubusercontent.com/RevoSucks/BMHeroRecomp/refs/heads/master/icons/app.png" },
@@ -91,27 +91,27 @@ namespace N64RecompLauncher.Services
 
             var custom = new List<object>
     {
-        new { name = "Zelda OoT (Ship of Harkinian)",
+        new { name = "Ship of Harkinian",
             repository = "harbourmasters/shipwright",
             folderName = "harbourmasters.shipofharkinian",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon/d1cd0a8c9b28f58703a097d5a25534e3/32/256x256.png" },
 
-        new { name = "Zelda MM (2 Ship 2 Harkinian)",
+        new { name = "2 Ship 2 Harkinian",
             repository = "harbourmasters/2ship2harkinian",
             folderName = "harbourmasters.2ship2harkinian",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon/6c7dbdd98cd70f67f102524761f3b4d2/24/256x256.png" },
 
-        new { name = "Star Fox 64 (Starship)",
+        new { name = "Starship",
             repository = "harbourmasters/starship",
             folderName = "harbourmasters.starship",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon/dc2ee2a5add7154447a4644326e33386/32/256x256.png" },
 
-        new { name = "Mario Kart 64 (SpaghettiKart)",
+        new { name = "SpaghettiKart",
             repository = "harbourmasters/spaghettikart",
             folderName = "harbourmasters.spaghettikart",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon_thumb/5e5e0bd5ad7c2ca72b0c5ff8b6debbba.png" },
 
-        new { name = "Super Mario 64 (Ghostship)",
+        new { name = "Ghostship",
             repository = "harbourmasters/ghostship",
             folderName = "harbourmasters.ghostship",
             gameIconUrl  = "https://raw.githubusercontent.com/HarbourMasters/Ghostship/refs/heads/develop/nx-logo.jpg" },
@@ -121,7 +121,7 @@ namespace N64RecompLauncher.Services
             folderName = "fgsfdsfgs.perfect_dark",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon/64314c17210c549a854f1f1c7adce8b6/32/256x256.png" },
 
-        new { name = "SM64 CoopDX",
+        new { name = "Super Mario 64: Coop Deluxe",
             repository = "coop-deluxe/sm64coopdx",
             folderName = "coop-deluxe.sm64coopdx",
             gameIconUrl  = "https://cdn2.steamgriddb.com/icon_thumb/e3dd863ef4277e82f712a5bd8fefe7d7.png" }
